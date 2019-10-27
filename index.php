ddc338b339cd2242fa11ee60c778083ad96505d5
Your Name <you@example.com>
2019-10-27 19:54:51 +0900

    firstcommit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+DEFINE("ACCESS_TOKEN","YDD9P7LxFOm4QRxH6k6vvNL4hiJdkZPZdXGr8biDB9RXWEX/RWCBl5JmIXQF/xexMMhDHHryHDiPPyUtiHgFPf7BtaIdK2bXUGTrITBa2loTalSN1zT3JxHBNtSmAQksGy/p4gRk8oHUwlQJXKvuPwdB04t89/1O/w1cDnyilFU=
+");
+DEFINE("SECRET_TOKEN","9478c29d783d6c8208ef8139da883d83");
+
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use \LINE\LINEBot\Constant\HTTPHeader;
+
+//LINESDKの読み込み
+require_once(__DIR__."/vendor/autoload.php");
+
+//LINEから送られてきたらtrueになる
+if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
+
+//LINEBOTにPOSTで送られてきた生データの取得
+  $inputData = file_get_contents("php://input");
+
+//LINEBOTSDKの設定
+  $httpClient = new CurlHTTPClient(ACCESS_TOKEN);
+  $Bot = new LINEBot($HttpClient, ['channelSecret' => SECRET_TOKEN]);
+  $signature = $_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE]; 
+  $Events = $Bot->parseEventRequest($InputData, $Signature);
+
+//大量にメッセージが送られると複数分のデータが同時に送られてくるため、foreachをしている。
+　　　　foreach($Events as $event){
+    $SendMessage = new MultiMessageBuilder();
+    $TextMessageBuilder = new TextMessageBuilder("よろぽん！");
+    $SendMessage->add($TextMessageBuilder);
+    $Bot->replyMessage($event->getReplyToken(), $SendMessage);
+  }
+}
+
