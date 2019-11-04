167da11dd6565b5bec470031bd03552bd9d43b4b
Your Name <you@example.com>
2019-11-04 22:56:05 +0900

    alcopy

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
+//Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__.'/vendor/autoload.php';
 
+// アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
 
+// LINE Messaging APIがリクエストに付与した署名を取得
 $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+// 不正であれば例外の内容を出力
 try {
     $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 }
@@ -22,20 +28,30 @@ catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
     error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
 }
 
-
+//配列に格納された各イベントをループで処理
 foreach ($events as $event) {
+  // MessageEventクラスのインスタンスでなければ処理をスキップ
     if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
         error_log('Non message event has come');
         continue;
     }
-
+// TextMessageBuilderクラスのインスタンスでなければ処理をスキップ
     if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
         error_log('Non text message has come');
         continue;
     }
     $message = array('type' => 'text',
                      'text' => 'こんにちは。テキスト応答ですよ。');
+//オウム返し
     $bot->replyText($event->getReplyToken(), $event->getText());
 }
 
+//画像を返信。引数はLINEBot、返信先、画像URL、サムネイルURL
+function replyImageMessage($bot,$replyToken,$originalImageUrl,$previewImageUrl){
+  // ImageMessageBuilderの引数は画像URL、サムネイルURL
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
+  if(!$response->isSucceeded()){
+    error_log('Failed! '. $response->getHTTPStatus . ' '.$response->getRawBody());
+  }
+}
 ?>
