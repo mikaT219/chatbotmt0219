3dd376512c2e7d11fb85c20163f83247ab13b3df
Your Name <you@example.com>
2019-11-04 23:05:06 +0900

    textrenew

--- index.php
+++ index.php
@@ -44,5 +44,17 @@ foreach ($events as $event) {
     $bot->replyText($event->getReplyToken(), $event->getText());
 }
 
+//テキストを返信。引数はLINEBot、返信先、テキスト
+function replyTextMessage($bot,$replyToken,$text) {
+  // 返信を行いメッセージを取得
+  // TextMessageBuilderの引数はテキスト
+  $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+  //レスポンスが異常な場合
+  if(!$response->isSucceeded()){
+    //エラー内容を出力
+    error_log('Failed! '. $response->getHTTPStatus . ' '.$response->getRawBody());
+  }
+}
 
 ?>
