da62db45c9c285abe3eb7df8ea935f38f8420658
Your Name <you@example.com>
2019-11-10 22:13:06 +0900

    connect

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-
+//host,user,pass,dbname
 $link = mysqli_connect('us-cdbr-iron-east-05.cleardb.net', 'bef176e47e8f17', 'd24f08d0', 'heroku_1ac9c94b4480f8f');
 
 // 接続状況をチェックします
@@ -8,3 +8,17 @@ if (mysqli_connect_errno()) {
 } else {
     echo "データベースの接続に成功しました。\n";
 }
+
+// userテーブルの全てのデータを取得する
+$query = "SELECT * FROM cardinfo;";
+
+// クエリを実行します。
+if ($result = mysqli_query($link, $query)) {
+    echo "SELECT に成功しました。\n";
+    foreach ($result as $row) {
+        var_dump($row);
+    }
+}
+
+// 接続を閉じます
+mysqli_close($link);
