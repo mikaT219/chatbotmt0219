412d2503ef3abcd41b346fd4110ccbfe49ea7ab2
Your Name <you@example.com>
2019-11-17 16:46:27 +0900

    test

---
+++ text.php
@@ -0,0 +1,60 @@
+<?php
+//Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/connect.php';
+
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+// 不正であれば例外の内容を出力
+try {
+    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+}
+catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
+    error_log("parseEventRequest failed. InvalidSignatureException => ".var_export($e, true));
+}
+catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
+    error_log("parseEventRequest failed. UnknownEventTypeException => ".var_export($e, true));
+}
+catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
+    error_log("parseEventRequest failed. UnknownMessageTypeException => ".var_export($e, true));
+}
+catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
+    error_log("parseEventRequest failed. InvalidEventRequestException => ".var_export($e, true));
+}
+
+//配列に格納された各イベントをループで処理
+foreach ($events as $event) {
+ // MessageEventクラスのインスタンスでなければ処理をスキップ
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
+        error_log('Non message event has come');
+        continue;
+
+ // TextMessageBuilderクラスのインスタンスでなければ処理をスキップ
+    if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
+        error_log('Non text message has come');
+        continue;
+
+  //テキストを返信し次のイベントへ
+      //replyTextMessage($bot, $event->getReplyToken(), $event->getText().'なの？');
+     replyTextMessage($bot, $event->getReplyToken(), $event->$res.'なの？');
+    }
+
+
+    //テキストを返信。引数はLINEBot、返信先、テキスト
+      // 返信を行いメッセージを取得
+      // TextMessageBuilderの引数はテキスト
+      $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+      //レスポンスが異常な場合
+      if(!$response->isSucceeded()){
+        //エラー内容を出力
+        error_log('Failed! '. $response->getHTTPStatus . ' '.$response->getRawBody());
+      }
+    }
