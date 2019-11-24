3aeb7f6be9e519216d8fec221c429259556a0a73
Your Name <you@example.com>
2019-11-24 15:49:17 +0900

    picture

--- connect.php
+++ connect.php
@@ -40,8 +40,8 @@ $query = "SELECT details FROM recmmend_table where id = $id;";
 if ($result = mysqli_query($link, $query)) {
     echo "SELECT に成功しました。\n";
     foreach ($result as $row) {
-        //$res = print_r($row,true);
-        $res = print(string $row);
+        $res = print_r($row,true);
+        //$res = print(string $row);
         echo $res;
     }
 }
