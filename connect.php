08f3b1c88c43f5d3c6a992820410213df38f97f6
Your Name <you@example.com>
2019-11-24 14:26:13 +0900

    picture

--- connect.php
+++ connect.php
@@ -39,9 +39,11 @@ $query = "SELECT DataColumn FROM cardinfo where id = $id;";
 if ($result = mysqli_query($link, $query)) {
     echo "SELECT に成功しました。\n";
     //foreach ($result as $row) {
-    foreach ($result as $row) {
-        $res = $id.'は'.print_r($row,true);
-        echo $res;
+        //$res = $id.'は'.print_r($row,true);
+        //echo $res;
+        foreach ($result as $id => $val) {
+            $res = '*'.$id.$val;
+            echo $res;
     }
 }
 
