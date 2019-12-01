0c192546b93f9592f108f7ecea88662bf316d320
Your Name <you@example.com>
2019-12-01 15:17:30 +0900

    feeling

--- index.php
+++ index.php
@@ -47,14 +47,14 @@ catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
       ////アクションの配列
       $actionArray = array();
       array_push ($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
-        'ボタン' .'Laughable'));
+        'ボタン' .'Laughable','Laughable'));
         //'ボタン' .$i . '-' . 1, 'c-' .$i .'-' . 1));
       array_push ($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
-        'ボタン' .'be impress'));
+        'ボタン' .'be impress','be impress'));
         //'ボタン' .$i . '-' . 2, 'c-' .$i .'-' . 2));
       array_push ($actionArray, new LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder(
         //'ボタン' .$i . '-' . 3, 'c-' .$i .'-' . 3));
-        'ボタン' .'acknowledg'));
+        'ボタン' .'acknowledg','acknowledg'));
       $column = new LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder (
         ($i + 1).'セレクト',
         '気分',
