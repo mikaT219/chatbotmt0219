665ed566aeae42023644a0cdb46f99f00448fbec
Your Name <you@example.com>
2019-12-08 23:52:51 +0900

    back

--- connect.php
+++ connect.php
@@ -11,6 +11,7 @@ $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATUR
 //host,user,pass,dbname
 $link = mysqli_connect('us-cdbr-iron-east-05.cleardb.net', 'bef176e47e8f17', 'd24f08d0', 'heroku_1ac9c94b4480f8f');
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
 // 接続状況をチェックします
 if (mysqli_connect_errno()) {
     die("データベースに接続できません:" . mysqli_connect_error() . "\n");
@@ -21,20 +22,17 @@ if (mysqli_connect_errno()) {
 //配列に格納された各イベントをループで処理
 foreach ($events as $event) {
   //テキストを返信し次のイベントへ
-$id = $event->getText();
-//$feeling = $event->getText();
-echo $id;
+  $id = $event->getText();
 }
-//$query = "SELECT title,details FROM recmmend_table where feeling LIKE 'relaxed';";
+
 $query = "SELECT title,details FROM recmmend_table where id = $id;";
-//$query = "SELECT title,details FROM recmmend_table WHERE feeling LIKE $id;";
+
 //$query = "SELECT title,details FROM recmmend_table WHERE feeling = $feeling ORDER BY rand() LIMIT 3;";
 // クエリを実行します。
 if ($result = mysqli_query($link, $query)) {
     //echo "SELECT に成功しました。\n";
     foreach ($result as $row) {
         $res = $row["title"].",".$row["details"]."\n";
-        echo $res;
     }
 }
 // 接続を閉じます
