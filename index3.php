65858ec685a407728f145d7d72c4bb846ea0291c
Your Name <you@example.com>
2019-12-15 20:20:14 +0900

    index3-genre_randamtest2

--- index3.php
+++ index3.php
@@ -58,7 +58,7 @@ foreach ($events as $event) {
     // 配列に格納された各イベントをループで処理
     // foreach ($events as $event) {
     //格納した返信をLINEに返す
-    $bot->replyText($event->getReplyToken(), $result);
+    $bot->replyText($event->getReplyToken(), $stmt->execute());
     // $bot->replyText($event->getReplyToken(), $event->getText());
     // $bot->replyText($event->getReplyToken(), $line_mes);
 }
