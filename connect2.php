80569ca599423d55e0440251339b70f75d45588e
Your Name <you@example.com>
2019-12-15 15:56:01 +0900

    index2connect2-sql3

--- connect2.php
+++ connect2.php
@@ -8,6 +8,10 @@ require_once __DIR__.'/vendor/linecorp/line-bot-sdk/src/LINEBot/Constant/HTTPHea
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 // CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+// LINE Messaging APIがリクエストに付与した署名を取得
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+// 署名が正当かチェック。正当であればリクエストをパースし配列へ
+$events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
 
 //DB接続
 $pdo = new PDO("mysql:dbname=heroku_1ac9c94b4480f8f;host=us-cdbr-iron-east-05.cleardb.net;charset=utf8","bef176e47e8f17","d24f08d0");
