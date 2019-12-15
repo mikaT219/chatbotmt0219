44263b9c45dd04d15e81e700024cdc5849270397
Your Name <you@example.com>
2019-12-15 15:52:33 +0900

    index2connect2-sql2

--- connect2.php
+++ connect2.php
@@ -1,7 +1,7 @@
 <?php
 //Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__.'/vendor/autoload.php';
-//require_once __DIR__.'/vendor/linecorp/line-bot-sdk/src/LINEBot/Event/MessageEvent/TextMessage.php';
+require_once __DIR__.'/vendor/linecorp/line-bot-sdk/src/LINEBot/Event/MessageEvent/TextMessage.php';
 require_once __DIR__.'/vendor/linecorp/line-bot-sdk/src/LINEBot/Constant/HTTPHeader.php';
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
@@ -13,14 +13,14 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 $pdo = new PDO("mysql:dbname=heroku_1ac9c94b4480f8f;host=us-cdbr-iron-east-05.cleardb.net;charset=utf8","bef176e47e8f17","d24f08d0");
 
 //ジャンルの格納
-// $id = 3;
-$id = $event->getText();
-//クエリの格納
-$stmt = $pdo->prepare("select title from recmmend_table where id = $id");
-
-//executeでクエリを実行
-$stmt->execute();
-
-// 結果をセット
-$result = $stmt->fetch();
-echo "title = ".$result['title'].PHP_EOL;
+foreach ($events as $event) {
+  // $id = 3;
+  $id = $event->getText();
+  //クエリの格納
+  $stmt = $pdo->prepare("select title from recmmend_table where id = $id");
+  //executeでクエリを実行
+  $stmt->execute();
+  // 結果をセット
+  $result = $stmt->fetch();
+  echo "title = ".$result['title'].PHP_EOL;
+}
