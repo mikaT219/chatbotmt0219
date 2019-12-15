ceb4e4d405e8da472642f187cb351e40cf38ca65
Your Name <you@example.com>
2019-12-15 19:53:20 +0900

    index3-genre_feeling6

--- index3.php
+++ index3.php
@@ -45,7 +45,8 @@ foreach ($events as $event) {
     // }
     //クエリの格納
     // $stmt = $pdo->prepare("select title from recmmend_table where id = $id;");
-    $stmt = $pdo->prepare("select title from recmmend_table where genre_feeling = $id;");
+    // $stmt = $pdo->prepare("select title from recmmend_table where genre_feeling = $id;");
+    $stmt = $pdo->prepare("select title from recmmend_table where genre_feeling = "."'".$id."'".";");
     $stmt_test = "select title from recmmend_table where genre_feeling = $id;";
     //executeでクエリを実行
     $stmt->execute();
